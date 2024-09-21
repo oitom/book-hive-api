24345d8466a964e62f17ad01bf9598f03b271c58
Wellington Costa <wcosta.ale@gmail.com>
2024-09-20 22:27:24 -0300

    chore(projeto) ajustes-organizacao-diretorios

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 header('Content-Type: application/json');
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/Presentation/RouteHandler.php';
+require_once __DIR__ . '/src/Presentation/Router/RouteHandler.php';
 
-use App\Presentation\RouteHandler;
+use App\Presentation\Router\RouteHandler;
 
 $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
--- src/Presentation/Router/RouteHandler.php
+++ src/Presentation/Router/RouteHandler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Presentation;
+namespace App\Presentation\Router;
 
 class RouteHandler
 {
