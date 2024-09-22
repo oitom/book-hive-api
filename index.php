b8d0ee4d23a56122612b96c2ee74136a88aa97fd
Wellington Costa <wcosta.ale@gmail.com>
2024-09-22 20:36:53 -0300

    feat(routes): desabilita-cors

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/Presentation/Routers/RouteHandler.php';
